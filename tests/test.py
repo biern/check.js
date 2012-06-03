bd8f64bea78b8e97e04574eea9cf1f364af9bcb5
biern <biern.m@gmail.com>
2012-06-03 20:51:32 +0200

    simple test update

--- tests/test.py
+++ tests/test.py
@@ -5,8 +5,22 @@ import sys
 sys.path.insert(0, '../src')
 
 from checkjs.parsers.antlr.parser import AntlrParser
+from checkjs.analyzers.ext import ExtAnalyzer
+from checkjs.analyzers.globals import GlobalsAnalyzer
+
 
 if __name__ == '__main__':
     p = AntlrParser()
     tree = p.parse_file(sys.argv[1])
-    p.print_tree_info(tree)
+
+    print('Checking "{0}"\n'.format(sys.argv[1]))
+
+    a = ExtAnalyzer()
+    a.analyze(tree)
+    a.print_result()
+
+    print('---')
+
+    a = GlobalsAnalyzer()
+    a.analyze(tree)
+    a.print_result()
